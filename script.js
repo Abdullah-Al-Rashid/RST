d48af61c53aa9ed11c62c83e0f47be7b45b9f6ff
Abdullah0505 <>
2021-05-19 12:48:40 +0000

    Revert to working state

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Connects calculator keys to the "numberToScreen" function
 document.getElementById('calculator-keys').addEventListener('click', numberToScreen)
 
-const screen = document.getElementById('calculator-screen')
+let screen = document.getElementById('calculator-screen')
 
 function numberToScreen (num) {
   // Delete last digit
-  const button = num.target.innerHTML
+  let button = num.target.innerHTML
   if (button === 'Del') {
-    const screen = (screen.innerHTML)
+    let screen = (screen.innerHTML)
     screen.innerHTML = onScreen.slice(0, -1)
   } else if (button === 'AC') {
-    const screen = (screen.innerHTML)
+    let screen = (screen.innerHTML)
     screen.innerHTML = onScreen.slice(0, -40)
   } else if (screen.innerHTML === '0') {
     screen.innerHTML = button
@@ -22,6 +22,7 @@ function numberToScreen (num) {
 
 document.getElementById('equal').addEventListener('click', result)
 function result () {
+  let onscreen = (screen.innerHTML)
   // Perform operation on inputted integers
-  screen.innerHTML = eval(screen)
+  screen.innerHTML = eval(onscreen)
 }
