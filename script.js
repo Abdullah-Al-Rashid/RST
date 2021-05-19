8c044248702d11dc46606a2e13a7a3366ba4ff76
Abdullah0505 <>
2021-05-19 15:01:30 +0000

    Revert to working state

--- script.js
+++ script.js
@@ -22,5 +22,5 @@ function numberToScreen (num) {
 document.getElementById('equal').addEventListener('click', result)
 function result () {
   const onscreen = (display.innerHTML)
-  display.innerHTML = Function('return ' + onscreen)();; // eslint-disable-line rule1 rule2
+  display.innerHTML = Function('return ' + onscreen)();; 
 }
